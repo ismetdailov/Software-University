4a820ab89d9a68ea351ac0a7da3512e7cf1db061
Ismet Dailov <ismetdailov@abv.bg>
2021-12-29 21:11:56 +0200

    MvcApplication
    C# WEB

---
+++ C# WEB/C# WEB Basics/SoftUni Server/Apps/MyFirstMvcApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyFirstMvcApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}

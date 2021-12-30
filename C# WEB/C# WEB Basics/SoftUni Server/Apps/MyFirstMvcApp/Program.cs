fb99972dc1eb291ff3d67ca123e76a68d80b3829
Ismet Dailov <ismetdailov@abv.bg>
2021-12-30 21:11:42 +0200

    WorkAboutHttpServer

--- C# WEB/C# WEB Basics/SoftUni Server/Apps/MyFirstMvcApp/Program.cs
+++ C# WEB/C# WEB Basics/SoftUni Server/Apps/MyFirstMvcApp/Program.cs
@@ -1,13 +1,33 @@
-﻿using System;
+﻿using SUS.HTTP;
+using System;
+using System.Threading.Tasks;
 
 namespace MyFirstMvcApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
+            IHttpServer server = new HttpServer();
+            server.AddRoute("/", HomePage);
+            server.AddRoute("/about", About);
+            server.AddRoute("/users/login", Login);
+            await server.StartAsync(80);
+        }
+        static HttpResponse HomePage(HttpRequest request)
+        {
+            throw new NotImplementedException();
+        }
+        static HttpResponse About(HttpRequest request)
+        {
+            throw new NotImplementedException();
+
+        }
+        static HttpResponse Login(HttpRequest request)
+        {
+            throw new NotImplementedException();
+
         }
     }
 }
---
+++ C# WEB/C# WEB Basics/SoftUni Server/SUS.HTTP/HttpResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUS.HTTP
+{
+  public  class HttpResponse
+    {
+    }
+}
